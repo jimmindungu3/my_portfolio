df2e60accf2c68acee68e5d316f03ff66fde4a48
jimmindungu3 <jimmindungu3@gmail.com>
2024-06-17 20:08:57 +0300

    animate roles

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// js
+const roles = [
+  "Software Developer",
+  "Network Engineer",
+  "Start-up ISP",
+  "TVET Trainer",
+];
+
+const roleHolder = document.querySelector(".roles");
+
+const typingSpeed = 125; //
+const erasingSpeed = 60; // Speed in milliseconds
+const delayBetweenRoles = 2000;
+let roleIndex = 0;
+let charIndex = 0;
+
+function typeRole() {
+  if (charIndex < roles[roleIndex].length) {
+    roleHolder.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeRole, typingSpeed);
+  } else {
+    setTimeout(eraseRole, delayBetweenRoles);
+  }
+}
+
+function eraseRole() {
+  if (charIndex > 0) {
+    roleHolder.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseRole, erasingSpeed);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(typeRole, typingSpeed);
+  }
+}
+
+// Start the typing effect
+document.addEventListener("DOMContentLoaded", () => {
+  typeRole();
+});
